449dfc40618ec73e953da85099298049c3b8b611
Rodrigosnrocha <83819959+Rodrigosnrocha@users.noreply.github.com>
2026-04-14 22:22:02 -0300

    Updated intro overlay

--- 2026_1_1_time_2/Assets/Scripts/Menu.cs
+++ 2026_1_1_time_2/Assets/Scripts/Menu.cs
@@ -8,11 +8,26 @@ public class Menu : MonoBehaviour
     [SerializeField] Animator animator;
     [SerializeField] WindowShrinking win;
     [SerializeField] GameObject player;
-    public void OnStartPress()
+    private bool started = false;
+    /*public void OnStartPress()
     {
         animator.SetTrigger("Start");
         win.StartShrinking();
         player.GetComponent<PlayerMovement>().enabled = true;
+    }*/
+
+    public void Update()
+    {
+        if (!started)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                animator.SetTrigger("Start");
+                win.StartShrinking();
+                player.GetComponent<PlayerMovement>().enabled = true;
+                started = true;
+            }
+        }
     }
     public void OnVolumeSliderChanged(float newValue) // valor 0.0 a 1.0
     {
