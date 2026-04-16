e73d8cf05047cf1bbb18500437c30f8168f17113
Henrique Carvalho <iquecarvalho2005@gmail.com>
2026-04-16 00:02:20 -0300

    Pop Ups System

--- 2026_1_1_time_2/Assets/Scripts/CloseUpManager.cs
+++ 2026_1_1_time_2/Assets/Scripts/CloseUpManager.cs
@@ -7,20 +7,70 @@ public class CloseUpManager : MonoBehaviour
 {
     [SerializeField] private CloseUp[] closeUps;
 
+    private Dictionary<string, CloseUp> closeUpDict;
+    private Stack<CloseUp> closeUpStack = new Stack<CloseUp>();
+    private CloseUp currentCloseUp;
+
+    private void Start()
+    {
+        closeUpDict = new Dictionary<string, CloseUp>();
+        foreach (CloseUp closeUp in closeUps) 
+        {
+            closeUpDict.Add(closeUp.name, closeUp);
+        }
+    }
+
     public void Open(string name) 
     {
-        
+        if (closeUpDict.TryGetValue(name, out CloseUp value))
+        {
+            OpenSimple(value);
+        }
+        else
+        {
+            print($"Wasn't Able to find closeUp {name}");
+        }
     }
 
     public void Close()
     {
-        
+        CloseSimple();
+        if (closeUpStack.Count > 0)
+        {
+            CloseUp nextCloseUp = closeUpStack.Pop();
+            OpenSimple(nextCloseUp);
+        }
+    }
+
+    private void OpenSimple(CloseUp closeUp) 
+    {
+        if (currentCloseUp != null) 
+        {
+            closeUpStack.Push(currentCloseUp);
+            CloseSimple();
+        }
+
+        currentCloseUp = closeUp;
+        currentCloseUp.closeUpObject.SetActive(true);
+    }
+
+    private void CloseSimple() 
+    {
+        if (currentCloseUp == null)
+        {
+            return;
+        }
+
+        currentCloseUp.closeUpObject.SetActive(false);
+        currentCloseUp = null;
+
+        return;
     }
 }
 
 [Serializable]
 public class CloseUp 
 {
-    [SerializeField] private string name;
-    [SerializeField] private GameObject closeUpObject;
+    public string name;
+    public GameObject closeUpObject;
 }
